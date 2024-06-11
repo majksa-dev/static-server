8055a2fea55c0e87e57aa70c3d0ca7bdd8b3052f
Ondřej Maxa <ondrej.maxa11@gmail.com>
2024-06-11 17:07:44 +0200

    feat: implement custom rust server library

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,16 +1,8 @@
-use std::{
-    net::{SocketAddr, TcpListener},
-    sync::Arc,
-};
-
-use essentials::info;
-
-use crate::{env::Env, utils};
+use crate::env::Env;
 
 use self::builder::Builder;
 
 mod builder;
-pub mod health_check;
 pub mod server;
 
 pub fn create() -> Builder {
@@ -19,18 +11,4 @@ pub fn create() -> Builder {
 
 pub fn build_from_env(env: &Env) -> Builder {
     create().load_env(env)
-}
-
-fn run_forever(context: Builder) {
-    let listener = TcpListener::bind(SocketAddr::from((context.host, context.port)))
-        .expect("Port is already in use");
-    let pool =
-        utils::thread::ThreadPool::new(context.threads).expect("Could not create thread pool");
-    let ctx = Arc::new(context);
-    let ctx2 = ctx.clone();
-    info!("HTTP server running at port {}", ctx.port);
-    std::thread::spawn(move || {
-        crate::http::health_check::run_forever(ctx);
-    });
-    server::run_forever(ctx2, listener, pool);
 }
